4ac5fc9e4da1528869574b96c13f8f7b0a9fa89d
Marc <marc.gourjon@tuhh.de>
2020-01-30 16:13:42 +0100

    first order secure implementations

--- first_order/asmgadgets_order1.s
+++ first_order/asmgadgets_order1.s
@@ -51,23 +51,20 @@ andOrder1:
 	ANDS    r5, r7
 	EORS    r5, r6
 	STR     r5, [r1, #0]
-	STR     r0, [r1, #4]
-	EORS    r4, r4
-	EORS    r6, r6
+	STR     r0, [r1, #4]    // clear_opW
+	EORS    r4, r4          // scrub(r4)
+	EORS    r6, r6          // scrub(r6)
 	LDR     r4, [r2, #4]
 	ANDS    r7, r4
-	LDR     r6, [r0, #4]
-	EORS    r7, r6
-        EORS    r5, r5
-	LDR     r5, [r3, #0]
+        LDR     r6, [r0, #4]
+        EORS    r6, r7
+        LDR     r5, [r3, #0]
 	ANDS    r5, r4
-	EORS    r7, r5
-	STR     r7, [r1, #4]
-        EORS    r5, r5
-        EORS    r6, r6
+	EORS    r6, r5
+	STR     r6, [r1, #4]
 	POP     {r4, r5, r6, r7}
-	ADDS    r0, r0, #4
-	STR     r0, [r0]
+	ADDS    r0, r0, #4      // clear_flags
+	STR     r0, [r0]        // clear_opW
 	BX      lr
 
 	.global refOrder1
@@ -80,15 +77,17 @@ andOrder1:
 refOrder1:
 	PUSH    {r4, r5}
 	LDR     r3, [r2, #0]
-	LDR     r4, [r0, #0]
+	LDR     r4, [r0, #4]
 	EORS    r3, r4
 	STR     r3, [r1, #0]
-	STR     r4, [r0, #0]
+	STR     r0, [r0, #0]    // clear_opW
 	LDR     r5, [r2, #4]
 	EORS    r5, r4
 	STR     r5, [r1, #4]
 	POP     {r4, r5}
-	ADDS    r0, r0, #4
+        EORS    r3, r3
+	ADDS    r0, r0, #4      // STMIA r0!, {r0}
+	STR     r0, [r0]
 	BX      lr
 
 	.global notOrder1
@@ -97,13 +96,15 @@ refOrder1:
         .thumb_func
         .type notOrder1 STT_FUNC
 	// notOrder1(uint32_t *entropy, uint32_t input[2])
-	// r0 entropy ptr, r1 *input1 ptr
+	// r0 *entropy, r1 *input1  == * output
         // in-place modification of input
 notOrder1:
 	LDR     r2, [r1, #0]
 	MVNS    r2, r2
 	STR     r2, [r1, #0]
- //       STR     r1, [sp, #-4]
+        LDR     r2, [r0, #0]    // scrub(r2), clear_opR
+        STR     r0, [r0, #0]    // clear_opW
+        ANDS    r0, r0          // clear flags, clear_opA, clear_opB
 	BX      lr
 
 	.global leakage
